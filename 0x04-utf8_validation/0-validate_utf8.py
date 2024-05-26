265c416e5e818e492a9a5db403e4c110df0a676f
karbal abdelhadi <abdoraja2005@gmail.com>
2024-05-26 18:26:37 +0100

    add function to handle number bigger than 255

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,15 +2,33 @@
 """scribt to check if data is a valid UTF-8"""
 
 
+def get_bytes(integer):
+    """
+    converts an integer to a list of its constituent bytes
+    """
+    if integer < 0:
+        raise ValueError("Value out of range")
+    bytes_list = []
+    while integer > 0:
+        bytes_list.insert(0, integer & 0xFF)
+        integer >>= 8
+    return bytes_list or [0]
+
+
 def validUTF8(data):
     """function to validate the data as utf-8"""
     # byte_representation = bytearray(xdata)
     # print(byte_representation)
+    # handle int greater than 255
+    byte_sequence = []
+    for number in data:
+        byte_sequence.extend(get_bytes(number))
+
     expected_continuation_bytes = 0
-    bin_data = []
-    for byte in data:
+    # bin_data = []
+    for byte in byte_sequence:
         if expected_continuation_bytes == 0:
-            binary_representation = bin(byte)  # [2:]
+            # binary_representation = bin(byte)  # [2:]
             if byte >> 5 == 0b110:
                 expected_continuation_bytes = 1
             elif byte >> 4 == 0b1110:
@@ -24,6 +42,6 @@ def validUTF8(data):
             if byte >> 6 != 0b10:
                 return False
             expected_continuation_bytes -= 1
-        bin_data.append(binary_representation)
+        # bin_data.append(binary_representation)
     # print(bin_data)
     return True
