ef9726a7b911bbba3cc4e6a96f17c7416f44aa69
karbal abdelhadi <abdoraja2005@gmail.com>
2024-06-14 02:15:56 +0100

    task0

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""scribt to  rotate a matrix 90 degrees clockwise."""
+
+
+
+def rotate_2d_matrix(matrix):
+    """rotate a matrix 90 degrees"""
+    rows = len(matrix)
+    cols = len(matrix[0])
+    for i in range(rows):
+        new_row = []
+        for j in range(cols):
+            new_row.append(matrix[j][i])
+        new_row.reverse()
+        matrix.append(new_row)
+    del matrix[:rows]
