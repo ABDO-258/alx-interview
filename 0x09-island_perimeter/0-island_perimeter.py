528f5555abde5a6ea3cbd46ad9eeb539ad001735
karbal abdelhadi <abdoraja2005@gmail.com>
2024-08-28 19:56:02 +0100

    comment out some depuging pront

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -17,23 +17,22 @@ def island_perimeter(grid):
             # print(element)
             if element == 1:
                 perimeter += 4
-                print(f"Land found at ({i},{j}), adding 4 to perimeter")
+                # print(f"Land found at ({i},{j}), adding 4 to perimeter")
                 # Check the cell above (i-1, j):
                 if i > 0 and grid[i-1][j] == 1:
-                    # If it's land, subtract 2 from the perimeter.
+                    # If it's land, subtract 1 from the perimeter.
                     perimeter -= 1
                 # Check the cell below (i+1, j):
                 if i < len(grid) - 1 and grid[i+1][j] == 1:
-                    # If it's land, subtract 2 from the perimeter.
+                    # If it's land, subtract 1 from the perimeter.
                     perimeter -= 1
                 # Check the cell to the left (i, j-1):
                 if j > 0 and grid[i][j-1] == 1:
-                    # If it's land, subtract 2 from the perimeter.
+                    # If it's land, subtract 1 from the perimeter.
                     perimeter -= 1
                 # Check the cell to the right (i, j+1):
                 if j < len(row) - 1 and grid[i][j+1] == 1:
-                    # If it's land, subtract 2 from the perimeter.
+                    # If it's land, subtract 1 from the perimeter.
                     perimeter -= 1
-                # If it's land, subtract 2 from the perimeter.
     # Return the total perimeter at the end.
     return perimeter
