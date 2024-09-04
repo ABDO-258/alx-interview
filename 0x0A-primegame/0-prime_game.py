6c7c8f179741c1c00d67298d01ffdcddae81c8e7
karbal abdelhadi <abdoraja2005@gmail.com>
2024-09-04 23:45:39 +0100

    find the winer game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""script for alx interview prime number game"""
+
+
+def is_prime(n):
+    """ check if n is a prime number """
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def isWinner(x, nums):
+    """define the winner in the game """
+    maria_wins = 0
+    ben_wins = 0
+    # Iterate through the given nums array
+    # representing the different rounds of the game.
+    for n in nums:
+        # initialize a list of available numbers from 1 to n
+        available_numbers = list(range(1, n + 1))
+        # whose turn it is (Maria or Ben).
+        maria_turn = True
+
+        while available_numbers:
+            if maria_turn:
+                for num in available_numbers[:]:
+                    if is_prime(num):
+                        available_numbers.remove(num)
+                        for i in range(num, n + 1, num):
+                            if i in available_numbers:
+                                available_numbers.remove(i)
+                        maria_turn = False
+                        break
+                else:
+                    ben_wins += 1
+                    break
+            else:
+                for num in available_numbers[:]:
+                    if is_prime(num):
+                        available_numbers.remove(num)
+                        for i in range(num, n + 1, num):
+                            if i in available_numbers:
+                                available_numbers.remove(i)
+                        maria_turn = True
+                        break
+                else:
+                    maria_wins += 1
+                    break
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
